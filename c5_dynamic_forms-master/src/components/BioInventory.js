453ee324a64abd3d338119ecc26607792f3fd883
bahayg <bahayg@gmail.com>
2020-07-31 22:49:26 -0700

    Edit the add  new inventory form

--- c5_dynamic_forms-master/src/components/BioInventory.js
+++ c5_dynamic_forms-master/src/components/BioInventory.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const BioInventory = (props) => {
-    const { name, observer, date, place } = props.info;
+    const { name, observer, date, place, id } = props.info;
     return (
         <div id="bioInvent">
-            <p><b>Name:</b> {name} <b>Observer:</b> {observer} <b>Date:</b> {date} <b>Place:</b> {place} </p>
+            <p><b>{id}:</b>  <b>Name:</b> {name} <b>Observer:</b> {observer} <b>Date:</b> {date} <b>Place:</b> {place} </p>
         </div>
     )
 }
--- c5_dynamic_forms-master/src/components/BioInventoryForm.js
+++ c5_dynamic_forms-master/src/components/BioInventoryForm.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import './bioInventory.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import { Divider } from '@material-ui/core';
 
 const BioInventoryForm = (props) => {
     return (
-        <form id="bioForm">
-            <TextField id="name" label="Name" variant="outlined" /> 
-            <TextField id="observer" label="Observer" variant="outlined" /> <Divider />
-            <TextField id="date" label="Date" variant="outlined" /> 
-            <TextField id="place" label="Place" variant="outlined" /> <Divider />
-            <TextField id="id" type="hidden" value={props.id} /> <Divider />
-            <Button type="submit" variant="contained" color="secondary"> Submit </Button>
+        <form onSubmit={(event) => props.handleSubmit(event)}>
+            <TextField id="name" label="Name" variant="outlined" /> <br/>
+            <TextField id="observer" label="Observer" variant="outlined" /> <br/>
+            <TextField id="date" label="Date" variant="outlined" /> <br/>
+            <TextField id="place" label="Place" variant="outlined" /> <br/>
+            <TextField id="id" type="hidden" value={props.id} />
+            <Button type="submit" variant="contained" color="secondary"> Submit </Button> <br/>
         </form>
     )
 }
