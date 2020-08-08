d97aa9f48dea3be8b65164d36c9f3655b1d3755f
bahayg <bahayg@gmail.com>
2020-08-08 16:21:45 -0700

    Insert correct answer as a random choice

--- c4_useEffect/src/components/Trivia.js
+++ c4_useEffect/src/components/Trivia.js
@@ -1,46 +1,56 @@
 import React, { useState, useEffect } from 'react';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 import Jumbotron from 'react-bootstrap/Jumbotron';
-import './trivia.css'
+// import './trivia.css'
 
 const API_URL = "https://opentdb.com/api.php?amount=30&category=17&difficulty=easy"
 
 const Trivia = () => {
-    const [triviaResults, setTriviaResults ] = useState ([]);
+    const [loadedQuestions, setLoadedQuestions ] = useState ([]);
 
     useEffect(() => {
         fetch(API_URL)
         .then(resp => resp.json())
-        .then(json => 
+        .then(json =>
+            // console.log(json.results) 
             {
-            setTriviaResults(json.results)
+            setLoadedQuestions(json.results)
         }
         )
     }, []);
+
+    const question = () => {
+        loadedQuestions.map(loadedQuestion => {
+            let formattedQuestion ={
+                question: loadedQuestion.question
+            };
+            let answerChoices = [ ...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random()*3) + 1;
+            answerChoices.splice(
+                formattedQuestion.answer - 1,
+                0,
+                loadedQuestion.correct_answer
+                );
+                answerChoices.forEach((choice, index) => {
+                    formattedQuestion['choice' + (index + 1)] = choice;
+                })
+                return formattedQuestion
+        })}
+    
 
     return (
         <>
         <Jumbotron>
             <h1>Welcome to Science Quiz!</h1>
-            <p>
-                Fun way of learning! {'\n'} You can learn more about science and nature through this website by answering the trivia questions.  
-            </p>
+            <p> Fun way of learning!  </p>
+            <p> The best trivia application on Science and Nature. </p> 
         </Jumbotron>
-        
+
         <div>
-            {triviaResults.map(trivia => (
-                <div>
-                <p id="question">{trivia.question} </p>
-                <p>{trivia.incorrect_answers.map(incorrectAns => (
-                    <p>{incorrectAns}</p>
-                ))} </p>
-                <p>{trivia.correct_answer}</p>
-                </div>
-            ))}
+            <p>{console.log(question)}</p>
         </div>
         </>
     )
-
 }
 
 export default Trivia;
