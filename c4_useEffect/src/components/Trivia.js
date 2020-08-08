9cda0bed2b0f3d82b2227da31d5c9497cbe7b524
bahayg <bahayg@gmail.com>
2020-08-08 11:48:44 -0700

    Install react-bootstrap and add jumbotron to Trivia.js

--- c4_useEffect/src/components/Trivia.js
+++ c4_useEffect/src/components/Trivia.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 import './trivia.css'
 
 const API_URL = "https://opentdb.com/api.php?amount=30&category=17&difficulty=easy"
@@ -17,6 +19,14 @@ const Trivia = () => {
     }, []);
 
     return (
+        <>
+        <Jumbotron>
+            <h1>Welcome to Science Quiz!</h1>
+            <p>
+                Fun way of learning! {'\n'} You can learn more about science and nature through this website by answering the trivia questions.  
+            </p>
+        </Jumbotron>
+        
         <div>
             {triviaResults.map(trivia => (
                 <div>
@@ -28,6 +38,7 @@ const Trivia = () => {
                 </div>
             ))}
         </div>
+        </>
     )
 
 }
